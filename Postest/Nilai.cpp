cce21c3aa6a5403cec6367d74273601877a0c770
eliott666 <akucantik97984@gmail.com>
2022-03-20 14:35:09 +0800

    Deteksi Kelulusan

---
+++ Postest/Nilai.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int nilai = 0;
+	
+	cout<<"Masukkan Nilai Kamu :";
+	cin>>nilai;
+
+	if (nilai >= 65){
+		cout<<"Selamat Anda LULUS!"<<endl;
+	}
+	else 
+	{
+		
+	cout<<"Maaf anda Belum LULUS!"<<endl;
+	
+}
+
+}
